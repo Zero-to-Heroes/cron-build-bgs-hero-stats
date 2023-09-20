de9ebe0a60dad1fee6115cd59377017a03a498d2
sebastientromp <sebastien.tromp@gmail.com>
2023-09-20 15:11:55 +0200

    save light version of hte hero stats

--- src/stats-v2/stats-v2.ts
+++ src/stats-v2/stats-v2.ts
@@ -4,8 +4,9 @@ import { gzipSync } from 'zlib';
 import { s3 } from '../build-battlegrounds-hero-stats-new';
 import { PatchInfo } from '../common';
 import { InternalBgsRow } from '../internal-model';
+import { WithMmrAndTimePeriod } from '../quests-v2/charged-stat';
 import { buildSplitStats } from '../quests-v2/data-filter';
-import { BgsHeroStatsV2 } from './bgs-hero-stat';
+import { BgsGlobalHeroStat, BgsHeroStatsV2 } from './bgs-hero-stat';
 import { buildStats } from './stats-buikder';
 
 export const STATS_BUCKET = `static.zerotoheroes.com`;
@@ -16,7 +17,7 @@ export const handleStatsV2 = async (
 	lastPatch: PatchInfo,
 	allCards: AllCardsService,
 ) => {
-	const rowsWithStats = rows.filter(row => !!row.rank).filter(r => !!r.tribes?.length);
+	const rowsWithStats = rows.filter((row) => !!row.rank).filter((r) => !!r.tribes?.length);
 	console.log('total relevant rows', rowsWithStats?.length);
 
 	const statResult = await buildSplitStats(rowsWithStats, timePeriod, lastPatch, (data: InternalBgsRow[]) =>
@@ -27,7 +28,7 @@ export const handleStatsV2 = async (
 		lastUpdateDate: new Date(),
 		mmrPercentiles: statResult.mmrPercentiles,
 		heroStats: stats,
-		dataPoints: stats.map(s => s.dataPoints).reduce((a, b) => a + b, 0),
+		dataPoints: stats.map((s) => s.dataPoints).reduce((a, b) => a + b, 0),
 	};
 	logger.log('\tbuilt stats', statsV2.dataPoints, statsV2.heroStats?.length);
 	const timeSuffix = timePeriod;
@@ -35,6 +36,38 @@ export const handleStatsV2 = async (
 		gzipSync(JSON.stringify(statsV2)),
 		STATS_BUCKET,
 		`api/bgs/stats-v2/bgs-${timeSuffix}.gz.json`,
+		'application/json',
+		'gzip',
+	);
+
+	// Build a "light" version
+	const statsV2light: BgsHeroStatsV2 = {
+		lastUpdateDate: new Date(),
+		mmrPercentiles: statResult.mmrPercentiles,
+		heroStats: statsV2.heroStats.map(
+			(s) =>
+				({
+					heroCardId: s.heroCardId,
+					dataPoints: s.dataPoints,
+					averagePosition: s.averagePosition,
+					// standardDeviation: s.standardDeviation,
+					// standardDeviationOfTheMean: s.standardDeviationOfTheMean,
+					// conservativePositionEstimate: s.conservativePositionEstimate,
+					// placementDistribution: s.placementDistribution,
+					// combatWinrate: s.combatWinrate,
+					// warbandStats: s.warbandStats,
+					// tribeStats: s.tribeStats,
+					// anomalyStats: s.anomalyStats,
+					mmrPercentile: s.mmrPercentile,
+					timePeriod: s.timePeriod,
+				} as Partial<WithMmrAndTimePeriod<BgsGlobalHeroStat>> as any),
+		),
+		dataPoints: statsV2.dataPoints,
+	};
+	await s3.writeFile(
+		gzipSync(JSON.stringify(statsV2light)),
+		STATS_BUCKET,
+		`api/bgs/stats-v2/light/bgs-${timeSuffix}.gz.json`,
 		'application/json',
 		'gzip',
 	);
